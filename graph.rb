abfc0bc89fe0559a0715374f8842f57b8b68b27f
stephtzhang <stephtzhang@gmail.com>
2014-12-29 16:10:21 -0500

    Add add_edge method call in make_edges

--- graph.rb
+++ graph.rb
@@ -1,16 +1,24 @@
 class Graph
-  def new(graph_info)
+  def new(graph_info, directed)
     @edges = {}
+    @directed = directed
     make_edges(graph_info)
   end
 
   def make_edges(graph_info)
-    graph_info.foreach do |node|
-      if @edges[node]
+    graph_info.foreach do |connection|
+      add_edge(connection[0], connection[1], @directed)
+    end
+  end
 
-      else
+  def add_edge(x, y, pair_processed?)
+    @edges[x]
+      @edges[x] << y
+    else
+      @edges[x] = [y]
+    end
 
-      end
+    add_edge(y, x, true) unless pair_processed?
   end
 end
 
@@ -20,4 +28,4 @@ class EdgeNode
 end
 
 graph_info = [[1, 2], [1, 3], [1, 4], [3, 4], [4, 5], [2,7], [5,6]];
-graph = Graph.new(graph_info)
+graph = Graph.new(graph_info, false)
