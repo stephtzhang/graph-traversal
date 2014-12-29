11146c7b64dd00783316f2d9ca10638eacefc691
stephtzhang <stephtzhang@gmail.com>
2014-12-29 16:02:00 -0500

    Add make_edges method

--- graph.rb
+++ graph.rb
@@ -4,8 +4,20 @@ class Graph
     make_edges(graph_info)
   end
 
+  def make_edges(graph_info)
+    graph_info.foreach do |node|
+      if @edges[node]
+
+      else
+
+      end
+  end
+end
+
 class EdgeNode
   def new()
   end
 end
 
+graph_info = [[1, 2], [1, 3], [1, 4], [3, 4], [4, 5], [2,7], [5,6]];
+graph = Graph.new(graph_info)
