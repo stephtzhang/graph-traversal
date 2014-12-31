4d8b5a876bf3f9effc6edd8aeaa6d3585125498f
stephtzhang <stephtzhang@gmail.com>
2014-12-31 12:38:16 -0500

    Update driver code

--- graph.rb
+++ graph.rb
@@ -155,14 +155,12 @@ class Graph
   end
 end
 
-graph_info = [[1, 2, 1], [1, 3, 1], [1, 4, 4], [3, 4, 1], [4, 5, 1], [2,7, 1], [5,6, 1], [8,9, 1]]
+graph_info = [[1, 2, 1], [1, 3, 1], [1, 4, 4], [3, 4, 1], [4, 5, 1], [2, 7, 1], [5, 6, 3], [8, 9, 1], [7, 6, 5]]
 graph = Graph.new(graph_info, false)
-# p graph.edges
-# graph.depth_first_search(1)
-# puts ""
-# graph.recursive_dfs(1)
-# puts ""
-# graph.breadth_first_search(1)
-# puts ""
-# graph.breadth_first_search(8)
+graph.depth_first_search(1)
+puts ""
+graph.recursive_dfs(1)
+puts ""
+graph.breadth_first_search(1)
+puts ""
 graph.dijkstra(1)
