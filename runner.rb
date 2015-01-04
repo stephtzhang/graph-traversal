e187606026ddc19c32f2f7e3e8ea2c1e1c2a0b36
stephtzhang <stephtzhang@gmail.com>
2015-01-04 08:48:35 -0500

    Separate long array to different lines for readability

--- runner.rb
+++ runner.rb
@@ -2,7 +2,8 @@ require_relative 'computer_info.rb'
 require_relative 'edge_node.rb'
 require_relative 'graph.rb'
 
-graph_info = [[1, 2, 1], [1, 3, 1], [1, 4, 4], [3, 4, 1], [4, 5, 1], [2, 7, 1], [5, 6, 3], [8, 9, 1], [7, 6, 5]]
+graph_info = [[1, 2, 1], [1, 3, 1], [1, 4, 4], [3, 4, 1], [4, 5, 1], [2, 7, 1],
+             [5, 6, 3], [8, 9, 1], [7, 6, 5]]
 graph = Graph.new(graph_info, false)
 graph.depth_first_search(1)
 puts ""
